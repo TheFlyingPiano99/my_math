a9e84863758843421a40c5f68da4b07b8a221b7e
TheFlyingPiano99 <57034327+TheFlyingPiano99@users.noreply.github.com>
2020-09-07 19:40:12 +0200

    deleted obsolate

--- my_functions.cpp
+++ my_functions.cpp
@@ -4,17 +4,3 @@
 
 #include "my_functions.h"
 
-///Multiplication:
-float dot_product(Vector4D a, Vector4D b) {
-    return a.x*b.x + a.y*b.y + a.z*b.z + a.w*b.w;
-}
-
-Vector4D cross_product(Vector4D a, Vector4D b) {
-    Vector4D res;
-    res.x = a.y * b.z - a.z * b.y,
-    res.y = a.z * b.x - a.x * b.z,
-    res.z = a.x * b.y - a.y * b.x;
-    res.w = 0;
-    return res;
-}
-
--- my_functions.h
+++ my_functions.h
@@ -5,51 +5,5 @@
 #ifndef LEGITARSASAG_FUNCTIONS_H
 #define LEGITARSASAG_FUNCTIONS_H
 
-#include <cmath>
-    class Functions {
-        double amp;
-        double omega1;
-        double omega2;
-    public:
-        Functions(double a = 1, double o1 = 1, double o2 = 1) : amp(a), omega1(o1), omega2(o2) {}
-
-        double sin(double t) {
-            return amp * std::sin(omega1 * t);
-        }
-
-        double cos(double t) {
-            return amp * std::cos(omega2 * t);
-        }
-
-        double sin_sin(double t) {
-            return amp * (std::sin(omega1 * t) + std::sin(omega2 * t));
-        }
-
-        void setAmp(double a) {
-            amp = a;
-        }
-
-
-        void setOmega1(double o) {
-            omega1 = o;
-        }
-
-        void setOmega2(double o) {
-            omega1 = o;
-        }
-    };
-
-#include "my_vector.h"
-
-///Transform:
-    float abs_vector(Vector4D k);
-
-    Vector4D norm_vector(Vector4D a);
-
-///Multiplication:
-    float dot_product(Vector4D a, Vector4D b);
-
-    Vector4D cross_product(Vector4D a, Vector4D b);
-
 
 #endif //LEGITARSASAG_FUNCTIONS_H
