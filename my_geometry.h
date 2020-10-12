ef263673519fb5e1bfc428399005f49610cb53af
TheFlyingPiano99 <57034327+TheFlyingPiano99@users.noreply.github.com>
2020-10-12 23:31:14 +0200

    Gauss elimination completted.

---
+++ my_geometry.h
@@ -0,0 +1,27 @@
+//
+// Created by simon on 2020. 10. 12..
+//
+
+#ifndef MY_MATH_MY_GEOMETRY_H
+#define MY_MATH_MY_GEOMETRY_H
+
+#include "my_vector.h"
+
+
+class Line3D {
+    Vector3D p;
+    Vector3D h;
+public:
+    Line3D(const Vector3D& p, const Vector3D& h):p(p), h(h) {}
+
+};
+class Plane3D {
+    Vector3D p;
+    Vector3D norm;
+public:
+    Plane3D(const Vector3D& p, const Vector3D& n): p(p), norm(n) {}
+};
+
+
+
+#endif //MY_MATH_MY_GEOMETRY_H
---
+++ my_special_relativity.h
@@ -0,0 +1,30 @@
+//
+// Created by simon on 2020. 10. 12..
+//
+
+#ifndef MY_MATH_MY_SPECIAL_RELATIVITY_H
+#define MY_MATH_MY_SPECIAL_RELATIVITY_H
+
+#import "my_vector.h"
+#import "my_matrix.h"
+
+class Event {
+    Vector4D p;
+    std::string name;
+public:
+    Event(const Vector4D& p, const std::string& n = "event"): p(p), name(n) {}
+};
+
+class Observer {
+
+public:
+    Observer() {}
+};
+
+class Spacetime {
+
+public:
+    Spacetime() {}
+};
+
+#endif //MY_MATH_MY_SPECIAL_RELATIVITY_H
